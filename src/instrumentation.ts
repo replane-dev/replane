d6cbfd8109fd0aa336eed26721fbdc8cb04e731f
tilyupo <tilyupo@gmail.com>
2025-09-11 13:38:18 +0300

    feat: update env variable names

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -2,10 +2,10 @@ export async function register() {
   if (!process.env.BASE_URL) {
     throw new Error('BASE_URL is not defined');
   }
-  if (!process.env.SECRET_KEY) {
-    throw new Error('SECRET_KEY is not defined');
+  if (!process.env.SECRET_KEY_BASE) {
+    throw new Error('SECRET_KEY_BASE is not defined');
   }
 
-  process.env.NEXTAUTH_SECRET = process.env.SECRET_KEY;
+  process.env.NEXTAUTH_SECRET = process.env.SECRET_KEY_BASE;
   process.env.NEXTAUTH_URL = process.env.BASE_URL;
 }
