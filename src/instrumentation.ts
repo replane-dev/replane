065872b2df3c526a25c312caa842827df3803dba
tilyupo <tilyupo@gmail.com>
2025-12-27 22:07:29 +0300

    feat: fail fast on invalid engine/edge config

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -2,7 +2,8 @@ import * as Sentry from '@sentry/nextjs';
 
 export async function register() {
   if (process.env.NEXT_RUNTIME === 'nodejs') {
-    await import('./init-node-environment');
+    const {init} = await import('./init-node-environment');
+    await init();
   }
 }
 
