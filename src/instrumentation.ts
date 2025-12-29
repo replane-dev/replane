aaf6b7848b4838e7ffb50d38e0c68c36fee9cdd9
tilyupo <tilyupo@gmail.com>
2025-12-29 15:04:41 +0300

    chore: fix build

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,12 +1,12 @@
 import * as Sentry from '@sentry/nextjs';
 
 export async function register() {
-  if (process.env.NEXT_RUNTIME !== 'nodejs') {
+  if (process.env.NEXT_RUNTIME === 'nodejs') {
+    const {init} = await import('./init-node-environment');
+    await init();
+  } else {
     throw new Error(`Unsupported NEXT_RUNTIME: ${process.env.NEXT_RUNTIME}`);
   }
-
-  const {init} = await import('./init-node-environment');
-  await init();
 }
 
 export const onRequestError = Sentry.captureRequestError;
